097db32b430f79f4bd99271008256896a16bcb20
Syed Nakib Hossain <snhossain@ebi.ac.uk>
2024-03-03 21:53:42 +0000

    Use cyvcf2 writer to avoid decode issue

--- nextflow/vcf_prepper/bin/summary_stats.py
+++ nextflow/vcf_prepper/bin/summary_stats.py
@@ -48,6 +48,7 @@ def parse_args(args = None, description: bool = None):
     return parser.parse_args(args)
 
 def minimise_allele(ref: str, alt: str) -> str:
+    minimised_allele_string = alt
     if ref[0] == alt[0]:
         minimised_allele_string = alt[1:] if len(alt) > 1 else "-" 
     return minimised_allele_string
@@ -56,118 +57,122 @@ def main(args = None):
     args = parse_args(args)
 
     input_file = os.path.realpath(args.input_file)
-    output_file = args.output_file or input_file.replace(".vcf.gz", "_renamed.vcf.gz")
+    output_file = args.output_file or "UPDATED_SS_" + input_file
 
-    with bgzf.open(output_file, "wt") as o_file:
-        input_vcf = VCF(input_file)
+    input_vcf = VCF(input_file)
+    # add to header and write header to output vcf
+    for header in HEADERS:
+        input_vcf.add_info_to_header(header)
 
-        # add to header and write header to output vcf
-        for header in HEADERS:
-            input_vcf.add_info_to_header(header)
-        o_file.write(input_vcf.raw_header)
+    output_vcf = Writer(output_file, input_vcf, mode="w")
+    # with bgzf.open(output_file, "wt") as o_file:
 
-        # parse csq header and get index of each field
-        csq_list = input_vcf.get_header_type("CSQ")['Description'].split("Format: ")[1].split("|")
-        csq_header_idx = {}
-        for index, value in enumerate(csq_list):
-            csq_header_idx[value] = index
         
-        # iterate through the file
-        for variant in input_vcf:
-            # create minimalized allele order
-            allele_order = []
-            ref = variant.REF
-            for alt in variant.ALT:
-                allele_order.append(minimise_allele(ref, alt))
+    # o_file.write(input_vcf.raw_header)
 
-            items_per_variant = {item: set() for item in PER_VARIANT_FIELDS}
-            items_per_allele = {}
+    # parse csq header and get index of each field
+    csq_list = input_vcf.get_header_type("CSQ")['Description'].split("Format: ")[1].split("|")
+    csq_header_idx = {}
+    for index, value in enumerate(csq_list):
+        csq_header_idx[value] = index
+    
+    # iterate through the file
+    for variant in input_vcf:
+        # create minimalized allele order
+        allele_order = []
+        ref = variant.REF
+        for alt in variant.ALT:
+            allele_order.append(minimise_allele(ref, alt))
 
-            # travers through each csq entry
-            csqs = variant.INFO["CSQ"]
-            for csq in csqs.split(","):
-                csq_values = csq.split("|")
-                
-                allele = csq_values[csq_header_idx["Allele"]]
-                if allele not in items_per_allele:
-                    items_per_allele[allele] = {item: set() for item in PER_ALLELE_FIELDS}
-                    
-                consequences = csq_values[csq_header_idx["Consequence"]]
-                feature_stable_id = csq_values[csq_header_idx["Feature"]]
-                for csq in consequences.split("&"):
-                    if csq in SKIP_CONSEQUENCE:
-                        continue
+        items_per_variant = {item: set() for item in PER_VARIANT_FIELDS}
+        items_per_allele = {}
 
-                    # genes
-                    gene = csq_values[csq_header_idx["Gene"]]               
-                    items_per_variant["gene"].add(gene)
-
-                    # regualtory and transcript consequences
-                    if csq.startswith("regulatory"):
-                        items_per_variant["regulatory_consequence"].add(f"{feature_stable_id}:{csq}")
-                    else:
-                        items_per_variant["transcipt_consequence"].add(f"{feature_stable_id}:{csq}")
-
-                # phenotype
-                if "PHENOTYPES" in csq_values:
-                    phenotypes = csq_values[csq_header_idx["PHENOTYPES"]]
-                    for phenotype in phenotypes.split("&"):
-                        pheno_PER_ALLELE_FIELDS = phenotype.split("+")
-                        if len(pheno_PER_ALLELE_FIELDS) != 3:
-                            continue
-                        
-                        (name, source, feature) = pheno_PER_ALLELE_FIELDS
-
-                        if feature.startswith("ENS"):
-                            items_per_allele[allele]["gene_phenotype"].add(f"{name}:{source}:{feature}")
-                        else:
-                            items_per_allele[allele]["variant_phenotype"].add(f"{name}:{source}:{feature}")
-
-                # citations
-                if "PUBMED" in csq_values:
-                    citations = csq_values[csq_header_idx["PUBMED"]]
-                    for citation in citations.split("&"):
-                        if citation != "":
-                            items_per_variant["citation"].add(citation)
-
-                # frequency
-                if "gnomAD_genomes_AF" in csq_values:
-                    frequency = csq_values[csq_header_idx["gnomAD_genomes_AF"]]
-                    if frequency != "":
-                        items_per_allele[allele]["frequency"] = frequency
-
-            # create summary info for per allele fields
-            for field in PER_ALLELE_FIELDS:
-                field_nums = []
-                for allele in allele_order:
-                    if allele in items_per_allele and field in items_per_allele[allele]:
-                        field_len = len(items_per_allele[allele][field])
-                        if field_len > 0:
-                            field_nums.append(str(field_len)) 
-
-                if field_nums:
-                    variant.INFO[PER_ALLELE_FIELDS[field]] = ",".join(field_nums)
-
-            # create summary info for frequency
-            field_vals = []
-            for allele in allele_order:
-                if allele in items_per_allele and "frequency" in items_per_allele[allele]:
-                    field_vals.append(items_per_allele[allele]["frequency"])
-                else:
-                    field_vals.append(".")
-
-                if not all(freq == "." for freq in field_vals):
-                    variant.INFO[FREQUENCY_FIELD] = ",".join(field_vals)
-
-            # create summary info for per variant fields
-            for field in PER_VARIANT_FIELDS:
-                field_len = len(items_per_variant[field])
-                if field_len > 0:
-                    variant.INFO[PER_VARIANT_FIELDS[field]] = str(field_len) 
-
-            o_file.write(str(variant))
+        # travers through each csq entry
+        csqs = variant.INFO["CSQ"]
+        for csq in csqs.split(","):
+            csq_values = csq.split("|")
             
-        input_vcf.close()
+            allele = csq_values[csq_header_idx["Allele"]]
+            if allele not in items_per_allele:
+                items_per_allele[allele] = {item: set() for item in PER_ALLELE_FIELDS}
+                
+            consequences = csq_values[csq_header_idx["Consequence"]]
+            feature_stable_id = csq_values[csq_header_idx["Feature"]]
+            for csq in consequences.split("&"):
+                if csq in SKIP_CONSEQUENCE:
+                    continue
+
+                # genes
+                gene = csq_values[csq_header_idx["Gene"]]               
+                items_per_variant["gene"].add(gene)
+
+                # regualtory and transcript consequences
+                if csq.startswith("regulatory"):
+                    items_per_variant["regulatory_consequence"].add(f"{feature_stable_id}:{csq}")
+                else:
+                    items_per_variant["transcipt_consequence"].add(f"{feature_stable_id}:{csq}")
+
+            # phenotype
+            if "PHENOTYPES" in csq_values:
+                phenotypes = csq_values[csq_header_idx["PHENOTYPES"]]
+                for phenotype in phenotypes.split("&"):
+                    pheno_PER_ALLELE_FIELDS = phenotype.split("+")
+                    if len(pheno_PER_ALLELE_FIELDS) != 3:
+                        continue
+                    
+                    (name, source, feature) = pheno_PER_ALLELE_FIELDS
+
+                    if feature.startswith("ENS"):
+                        items_per_allele[allele]["gene_phenotype"].add(f"{name}:{source}:{feature}")
+                    else:
+                        items_per_allele[allele]["variant_phenotype"].add(f"{name}:{source}:{feature}")
+
+            # citations
+            if "PUBMED" in csq_values:
+                citations = csq_values[csq_header_idx["PUBMED"]]
+                for citation in citations.split("&"):
+                    if citation != "":
+                        items_per_variant["citation"].add(citation)
+
+            # frequency
+            if "gnomAD_genomes_AF" in csq_values:
+                frequency = csq_values[csq_header_idx["gnomAD_genomes_AF"]]
+                if frequency != "":
+                    items_per_allele[allele]["frequency"] = frequency
+
+        # create summary info for per allele fields
+        for field in PER_ALLELE_FIELDS:
+            field_nums = []
+            for allele in allele_order:
+                if allele in items_per_allele and field in items_per_allele[allele]:
+                    field_len = len(items_per_allele[allele][field])
+                    if field_len > 0:
+                        field_nums.append(str(field_len)) 
+
+            if field_nums:
+                variant.INFO[PER_ALLELE_FIELDS[field]] = ",".join(field_nums)
+
+        # create summary info for frequency
+        field_vals = []
+        for allele in allele_order:
+            if allele in items_per_allele and "frequency" in items_per_allele[allele]:
+                field_vals.append(items_per_allele[allele]["frequency"])
+            else:
+                field_vals.append(".")
+
+            if not all(freq == "." for freq in field_vals):
+                variant.INFO[FREQUENCY_FIELD] = ",".join(field_vals)
+
+        # create summary info for per variant fields
+        for field in PER_VARIANT_FIELDS:
+            field_len = len(items_per_variant[field])
+            if field_len > 0:
+                variant.INFO[PER_VARIANT_FIELDS[field]] = str(field_len) 
+
+        output_vcf.write_record(variant)
+        
+    input_vcf.close()
+    output_vcf.close()
     
 if __name__ == "__main__":
     sys.exit(main())
--- nextflow/vcf_prepper/modules/local/summary_stats.nf
+++ nextflow/vcf_prepper/modules/local/summary_stats.nf
@@ -8,13 +8,15 @@ process SUMMARY_STATS {
   tuple val(meta), path(output_file), path(vcf_index)
   
   shell:
-  output_file =  "UPDATED_" + file(vcf).getName()
+  output_file =  "UPDATED_SS_" + file(vcf).getName()
+  uncomressed_output_file = output_file.replaceFirst(/.gz$/, "")
   index_type = meta.index_type
   flag_index = (index_type == "tbi" ? "-t" : "-c")
   vcf_index = output_file + ".${index_type}"
 
   '''
-  summary_stats.py !{vcf} -O !{output_file}
+  summary_stats.py !{vcf} -O !{uncomressed_output_file}
+  bgzip !{uncomressed_output_file}
   bcftools index !{flag_index} !{output_file}
   '''
 }
