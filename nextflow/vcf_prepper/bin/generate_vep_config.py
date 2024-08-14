5c0b6717504545154aabd91541feac753f15fd32
Syed Nakib Hossain <snhossain@ebi.ac.uk>
2024-08-14 15:00:17 +0100

    Update CADD version to v1.7

--- nextflow/vcf_prepper/bin/generate_vep_config.py
+++ nextflow/vcf_prepper/bin/generate_vep_config.py
@@ -178,8 +178,11 @@ def get_plugin_args(
         plugin_data_dir = plugin_data_dir.replace("grch38", "grch37")
         
     if plugin == "CADD":
-        snv = os.path.join(plugin_data_dir, f"CADD_{assembly}_1.6_whole_genome_SNVs.tsv.gz")
-        indels = os.path.join(plugin_data_dir, f"CADD_{assembly}_1.6_InDels.tsv.gz")
+        # CADD have data v1.7 data file from e113
+        if version < 113:
+            plugin_data_dir = plugin_data_dir.replace(f"{version}", "113")
+        snv = os.path.join(plugin_data_dir, f"CADD_{assembly}_1.7_whole_genome_SNVs.tsv.gz ")
+        indels = os.path.join(plugin_data_dir, f"CADD_{assembly}_1.7_InDels.tsv.gz")
         
         check_plugin_files(plugin, [snv, indels])
         
