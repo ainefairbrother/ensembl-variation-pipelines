751c39372d589c6e023e671bc98d9e0fefd5a8e1
Aine <fairbrot@ebi.ac.uk>
2025-04-25 15:52:32 +0100

    fix: revert remove -o

--- scripts/auto_create_input_config.py
+++ scripts/auto_create_input_config.py
@@ -73,7 +73,16 @@ def parse_args(args=None):
         required=False,
         help="Config file with database server information",
     )
-    
+    parser.add_argument(
+        "-O",
+        "--output_file",
+        dest="output_file",
+        type=str,
+        default="input_config.json",
+        required=False,
+        help="Full path to output file",
+    )
+
     return parser.parse_args(args)
 
 
