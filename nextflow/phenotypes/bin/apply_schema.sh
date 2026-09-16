da3a5091030a38681dfee1427348106df29fbda6
Aine <fairbrot@ebi.ac.uk>
2026-09-16 11:43:34 +0100

    docs: note in apply schema script

--- nextflow/phenotypes/bin/apply_schema.sh
+++ nextflow/phenotypes/bin/apply_schema.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
+# assumes PGSCHEMA has been initiated in db, and then set in .env 
 set -a; source .env; set +a
-
 psql -X -v ON_ERROR_STOP=1 -c "SET search_path TO ${PGSCHEMA}" -f sql/schema.sql
